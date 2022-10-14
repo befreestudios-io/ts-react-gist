d41d54def4aab5a9d7d7301bb221c987227810e0
Burton Podczerwinski <burtonp@slalom.com>
2022-10-14 14:23:16 -0500

    coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,12 @@ module.exports = {
     coverageDirectory: 'coverage',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    coverageThreshold: {
+        "global": {
+          "lines": 90,
+          "branches": 50,
+          "functions": 90,
+          "statements": 90
+        }
+      }
 }
